3cf1c32cc4040292db77bb6c5e16554fdab0f7d3
Stephen Carlson <stephen.carlson@bloomreach.com>
2015-04-24 17:47:15 -0400

    My updated changes for today on the moovweb igagdet website

--- Moovweb_Projects/stephen_moovweb_firstProj/scripts/pages/home.ts
+++ Moovweb_Projects/stephen_moovweb_firstProj/scripts/pages/home.ts
@@ -1,4 +1,20 @@
 $$("body") {
 	# Adding a class for page-specific styling
 	add_class("_home")
+
+	$("./div[@id='Container']/div[@id='Outer']") {
+    $("./div[@id='Wrapper']") {
+      $("./div[@class='Left']") {
+        remove()
+      }
+      $("./div[@class='Content']") {
+        $("./div[@id='HomeFeaturedProducts']") {      
+        }  
+      }
+    }
+  }
+}
+# Removes all RSS image spans
+$("//span[contains (@class, 'FeedLink')]") {
+  remove()
 }
